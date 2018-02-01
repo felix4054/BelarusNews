a963f17d8a435e04250bc0bec13996c4a8235d0e
Aliaksandr Kavalchuk <avkavalchuk@gmail.com>
2018-02-01 19:11:10 +0300

    01/02/2018

--- app/src/main/java/kavalchuk/aliaksandr/belarusnews/Fragment/AllNewsFragment.kt
+++ app/src/main/java/kavalchuk/aliaksandr/belarusnews/Fragment/AllNewsFragment.kt
@@ -14,7 +14,7 @@ class AllNewsFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
